712c7040964404ca9c2764d6db038915ce3ce4aa
RainneMagno <rainnemagno@gmail.com>
2025-01-27 22:17:17 +0800

    Add files via upload

---
+++ PersonalDetails.java
@@ -0,0 +1,16 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        String name = "Rainne E. Magno";
+        int age = 17;
+        double height = 1.85;
+        boolean isStudent = true;
+ 
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+        System.out.println("Is a Student? " + isStudent);
+
+        System.out.println("Hi, my name is " + name + ". I am " + age + " years old, " + height + " meters tall, and it is " + isStudent + " that I am a student.");
+    }
+    
+}
